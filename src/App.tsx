4168e2e3f84019cfbe2e8e61ab44192505699e74
sh.dzhanhotov <sh.dzhanhotov@it-only.ru>
2023-05-28 17:57:55 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
         >
           Learn React
         </a>
